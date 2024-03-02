3d0905fcaebad084a604e1d9e3127047f6a7827d
adityamoney <adityabhadauriya93@gmail.com>
2024-03-02 15:52:50 +0530

    edit and search

--- trade/item/views.py
+++ trade/item/views.py
@@ -1,10 +1,26 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Item
-from .forms import NewItemForm
+from .models import Item, Category
+from .forms import NewItemForm, EditItemForm
+from django.urls import reverse
+from django.db.models import Q
 
 
 # Create your views here.
+
+def items(request):
+    query = request.GET.get('query','')
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold = False)
+    
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    
+    return render(request, 'item/items.html', {
+        'items': items,
+         'query': query,
+         'categories': categories,
+    })
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -36,3 +52,37 @@ def new(request):
         'form': form,
         'title': 'New item',
     })
+    
+
+@login_required
+
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+    
+    return redirect('dashboard:index')
+
+
+@login_required
+def edit(request,pk):
+    # print("Edit view is being executed.")
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    # edit_url = reverse('item:edit', kwargs={'pk': pk})
+    # print(f"Generated Edit URL: {edit_url}")
+    
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('item:detail',pk=item.id)
+    else: 
+        form = EditItemForm(instance=item)
+    
+    
+    return render(request, 'item/form.html',{
+        'form': form,
+        'title': 'Edit item',
+    })
+    
